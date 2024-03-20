238ee708b5d4d1c749fc4ef76fb57cef874c15eb
Jason Kretzer <JasonKretzer@user.noreply.github.com>
2024-03-20 16:57:08 -0400

    Replaced the Blog Post body text area with ActionText
    Added a rich text editor to the blog post edit
    Changed the default sort on the index

--- spec/factories/blog_post.rb
+++ spec/factories/blog_post.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   # creates a `published` blog post by default
   factory :blog_post do
     title { Faker::Company.bs }
-    body { Faker::TvShows::TheITCrowd.quote }
+    content { Faker::TvShows::TheITCrowd.quote }
     published_at { Time.current - 1.day }
   end
 
