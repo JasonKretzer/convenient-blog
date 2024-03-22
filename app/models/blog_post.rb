ea6e80030ca3ce680096a8c1acc8753c1436f826
Jason Kretzer <JasonKretzer@user.noreply.github.com>
2024-03-22 10:21:23 -0400

    Cover Images for Blog Posts
    Added ability to add/remove cover images for blog posts
    Did some basic styling on the edit form to make it a little nicer to
    look at

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,6 @@
 class BlogPost < ApplicationRecord
+  # these are action_storage and action_context, respectively
+  has_one_attached :cover_image
   has_rich_text :content
 
   validates :title, presence: true
